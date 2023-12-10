457580f1f29d94e6954ac564d5f3ece2322bbd3c
Abhishek <21bcs116@nith.ac.in>
2023-12-11 00:20:18 +0530

    IPFS_Integration :fire::white_check_mark: mintFuntion:alien:

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,10 +1,38 @@
 import React, { useState, useRef, useEffect } from "react";
 import FileSelectButton from "@/components/FileSelectButton/FileSelectButton";
 import "./share.css";
+import {
+  useWeb3ModalProvider,
+  useWeb3ModalAccount,
+} from "@web3modal/ethers5/react";
+import abi from '../../app/home/utils/memeplace.json'
+// IPFS
+
+import { ethers } from "ethers";
+import { Buffer } from "buffer";
+import { create as ipfsHttpClient } from "ipfs-http-client";
+const projectId = "2ONjCGu7UlrPOzmZ3hqy8WlN2GC";
+const projectSecretKey = "43cc6a424bd74fd70d8a175972fbba87";
+
+const auth = `Basic ${Buffer.from(`${projectId}:${projectSecretKey}`).toString(
+  "base64"
+)}`;
+const subdomain = "https://uniqo-marketplace.infura-ipfs.io";
+const client = ipfsHttpClient({
+  host: "infura-ipfs.io",
+  port: 5001,
+  protocol: "https",
+  headers: {
+    authorization: auth,
+  },
+});
 
 export default function Share() {
+  const [imagei, setImage] = useState("");
+  const [url , setUrl] = useState("");
   const [selectedFile, setSelectedFile] = useState(null);
   const [selectedEmoji, setSelectedEmoji] = useState(null);
+  const [name ,setName] = useState('');
   const canvasRef = useRef(null);
   const [text, setText] = useState("");
   const [textPosition, setTextPosition] = useState({ x: 80, y: 50 });
@@ -16,14 +44,38 @@ export default function Share() {
   const [emojiScale, setEmojiScale] = useState(0.1);
   const [isDraggingEmoji, setIsDraggingEmoji] = useState(false);
   const [dragStartEmoji, setDragStartEmoji] = useState({ x: 0, y: 0 });
-  const [price, setPrice] = useState(0);
+  const [price, setPrice] = useState('');
   const [description, setDescription] = useState("");
+  const [listingPrice, setListingPrice] = useState("");
+
+  //   contract fetching starts here------------------------------
+
+  const { address, chainId, isConnected } = useWeb3ModalAccount();
+  const { walletProvider } = useWeb3ModalProvider();
+
+  const Address = "0x4dA194bC069bDf5a5ee580632dAF0b986b45287f";
+
+  async function getContract() {
+    if (!isConnected) {
+      alert("Please connect Metamask");
+      return;
+    }
+    const ethersProvider = new ethers.providers.Web3Provider(walletProvider);
+    const signer = await ethersProvider.getSigner();
+    // The Contract object
+    const contract = new ethers.Contract(Address, abi, signer);
+    return contract;
+  }
+  // ends here ----------------------------------------------------------
 
   const handlePriceChange = (event) => {
     setPrice(event.target.value);
   };
   const handleDescriptionChange = (event) => {
     setDescription(...event.target.value);
+  };
+  const handleNameChange = (event) => {
+    setName(...event.target.value);
   };
 
   useEffect(() => {
@@ -100,23 +152,92 @@ export default function Share() {
     setTextScale(1);
   };
 
-  const handleMint = () => {
+  const UploadtoIpfs = async () => {
     const canvas = canvasRef.current;
 
     const dataURL = canvas.toDataURL();
 
-    fetch(dataURL)
-      .then((res) => res.blob())
-      .then((blob) => {
-        const file = new File([blob], "meme.png", { type: "image/png" });
-         
+    try {
+      const blob = await fetch(dataURL).then((res) => res.blob());
+      const file = new File([blob], "meme.png", { type: "image/png" });
 
-        console.log("Minting with the created meme file:", file);
-      })
-      .catch((error) => {
-        console.error("Error converting data URL to Blob:", error);
-      });
+      if (typeof file !== "undefined") {
+        try {
+          const result = await client.add(file);
+          console.log("this is result", result);
+          setImage(`${subdomain}/ipfs/${result.path}`);
+        console.log(imagei);
+        } catch (error) {
+          console.log("ipfs image upload error: ", error);
+        }
+      }
+
+      console.log("Minting with the created meme file:", file);
+    } catch (error) {
+      console.error("Error converting data URL to Blob:", error);
+    }
   };
+
+  const handleMint = async () => {
+     var contract ;
+	try {
+		contract = await getContract();
+
+	} catch (error) {
+		alert("error in fetching contract")
+	}
+   
+    if (!contract) {
+      alert("Error,Try again by conneting metamask");
+      return;
+    }
+    const imageURL = UploadtoIpfs();
+	const Price = ethers.utils.parseUnits(price, 'ether');
+
+	const data = JSON.stringify({name, description, image: imagei});
+     
+
+	try {
+		const result = await client.add(data);
+        setUrl(`${subdomain}/ipfs/${result.path}`);
+	} catch (error) {
+		alert("Error, in file uplaod on ipfs")
+		return;
+	}
+
+	if(!url || !Price){
+		alert("Fill all the inputs");
+		return;
+	}
+    
+
+	try {
+		const listing = await contract.getListingPrice();
+		console.log(listing);
+		setListingPrice(listing);
+	} catch (error) {
+		alert("error in fetching the price ")
+	}
+
+	if(!listingPrice){
+		alert("listing price error")
+		return;
+	}
+
+	console.log(url, Price,listingPrice.toString())
+
+	try {
+		const transaction = await contract.createToken(url, Price, { value: listingPrice.toString() })
+        await transaction.wait();
+	} catch (error) {
+		alert("Error in creating Token  , try Again");
+	}
+
+  };
+
+
+
+
 
   const handleMouseDownText = (e) => {
     setIsDraggingText(true);
@@ -224,6 +345,14 @@ export default function Share() {
                 onChange={handlePriceChange}
                 className="priceInput"
                 placeholder="price"
+              />
+
+              <input
+                type="text"
+                value={name}
+                onChange={handleNameChange}
+                className="priceInput"
+                placeholder="name"
               />
             </div>
 
